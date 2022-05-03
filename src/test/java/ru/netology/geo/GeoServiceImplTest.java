7aad199288e07190437b9d36ed9ae5ad33ad3dfc
VladimirZak <v.v.zak@list.ru>
2022-05-03 23:40:15 +0800

    fixed some bugs

--- src/test/java/ru/netology/geo/GeoServiceImplTest.java
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -1,5 +1,5 @@
 package ru.netology.geo;
-import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import ru.netology.entity.Country;
@@ -13,18 +13,18 @@ class GeoServiceImplTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"172.0.32.11", "172."})
-    void  testVerify_byIp_RU(String moscowIP) {
+    void testVerify_byIp_RU(String moscowIP) {
 
-    GeoServiceImpl geoService = new GeoServiceImpl();
-    Location location = geoService.byIp(moscowIP);
-    Country result = location.getCountry();
-    assertNotNull(result);
-    assertEquals(RUSSIA, result);
+        GeoServiceImpl geoService = new GeoServiceImpl();
+        Location location = geoService.byIp(moscowIP);
+        Country result = location.getCountry();
+        assertNotNull(result);
+        assertEquals(RUSSIA, result);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"96.44.183.149", "96."})
-    void  testVerify_byIp_USA(String newYorkIP) {
+    void testVerify_byIp_USA(String newYorkIP) {
 
         GeoServiceImpl geoService = new GeoServiceImpl();
         Location location = geoService.byIp(newYorkIP);
--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -1,10 +1,8 @@
 package ru.netology.sender;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
